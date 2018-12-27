5b60e2358e042c5809a0feeebc3385ee364ae825
Kolya Grebnev <mumiyasss@gmail.com>
2018-12-27 13:38:37 +0300

    Messages List initial

--- lib/ui/chatscreen/ChatScreenView.dart
+++ lib/ui/chatscreen/ChatScreenView.dart
@@ -5,23 +5,84 @@ class ChatScreenView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            Center(
-              child: RaisedButton(
-                  child: new Text('dismiss'),
-                  onPressed: () {
-                    FocusScope.of(context).requestFocus(new FocusNode());
-                    SingleChildScrollView();
-                  }),
+            Flexible(
+              child: GestureDetector(
+                onTap: () =>
+                    FocusScope.of(context).requestFocus(new FocusNode()),
+                child: Container(
+                  height: 600,
+                  child: ListView(
+                    reverse: true,
+                    children: <Widget>[
+                      MessageBubble(false, "Hello"),
+                      MessageBubble(true, "Hello"),
+                      MessageBubble(
+                          false, "Hello, how are you, buddy? I heard "),
+                      MessageBubble(false,
+                          "Hello, how are you, buddy? I heard a lot about you!!!"),
+                      MessageBubble(true,
+                          "Hello, how are you, buddy? I heard a lot about you!!!"),
+                      MessageBubble(true,
+                          "Hello, how are you, buddy? I heard a lot about you!!!"),
+                      MessageBubble(true,
+                          "Hello, how are you, buddy? I heard a lot about you!!!"),
+                      MessageBubble(false,
+                          "Hello, how are you, buddy? I heard a lot about you!!!"),
+                      MessageBubble(true,
+                          "Hello, how are you, buddy? I heard a lot about you!!!"),
+                      MessageBubble(false,
+                          "Hello, how are you, buddy? I heard a lot about you!!!"),
+                    ],
+                  ),
+                ),
+              ),
             ),
             MessageField()
           ],
         ),
       ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble(this.self, this.text);
+  final bool self;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    var bigMargin = MediaQuery.of(context).size.width * 0.3;
+    var bottomMargin = 9.0;
+
+    var message = Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        border: Border.all(color: Color(0xFFCECECE)),
+        color: self ? Color(0xFFFFFFFF) : Color(0xFF2196F3),
+      ),
+      child: Text(
+        text,
+        textAlign: TextAlign.justify,
+        // textAlign: self ? TextAlign.end : TextAlign.start,
+        style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+            color: self ? Colors.black : Colors.white),
+      ),
+    );
+
+    return Container(
+      padding: self
+          ? EdgeInsets.only(left: bigMargin, right: 10, bottom: bottomMargin)
+          : EdgeInsets.only(right: bigMargin, left: 10, bottom: bottomMargin),
+      alignment: self ? Alignment.centerRight : Alignment.centerLeft,
+      child: message,
     );
   }
 }
