05ec97044c8a0ddb9f1fbd4b4a9eb41bc1521b18
Kolya Grebnev <mumiyasss@gmail.com>
2019-05-30 15:10:39 +0300

    packages update, correction in sign in method due to FirebaseAuth library update. Start moving to BLOC pattern.

--- lib/ui/signin/SingInViewModel.dart
+++ lib/ui/signin/SingInViewModel.dart
@@ -1,41 +1,50 @@
+import 'dart:async';
+
 import 'package:catch_me/main.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class SignInViewModel {
-  final _gSignIn = GoogleSignIn();
+    final _gSignIn = GoogleSignIn();
 
-  void googleSignIn() async {
-    GoogleSignInAccount googleSignInAccount = await _gSignIn.signIn();
-    GoogleSignInAuthentication authentication =
-        await googleSignInAccount.authentication;
-    await FirebaseAuth.instance.signInWithGoogle(
-        idToken: authentication.idToken,
-        accessToken: authentication.accessToken);
-    var user = await initUserId();
-    var userToCheck = (await Firestore.instance
-        .document('users/' + CatchMeApp.userUid)
-        .get());
-    var userDoesNotExists = userToCheck.data == null;
-    if (userDoesNotExists) {
-      Firestore.instance.document('users/' + CatchMeApp.userUid).setData({
-        'id': CatchMeApp.userUid,
-        'name': user.displayName,
-        'email': user.email,
-        'phone': user.phoneNumber,
-        'photo': user.photoUrl
-      });
+    void googleSignIn() async {
+        final googleCredential = await _getGoogleCredential();
+        await FirebaseAuth.instance.signInWithCredential(googleCredential);
+        var user = await initUserId();
+        var userToCheck = (await Firestore.instance
+            .document('users/' + CatchMeApp.userUid)
+            .get());
+        var userDoesNotExists = userToCheck.data == null;
+        if (userDoesNotExists) {
+            Firestore.instance.document('users/' + CatchMeApp.userUid).setData({
+                'id': CatchMeApp.userUid,
+                'name': user.displayName,
+                'email': user.email,
+                'phone': user.phoneNumber,
+                'photo': user.photoUrl
+            });
+        }
     }
-  }
 
-  Future<FirebaseUser> initUserId() async {
-    var user = await FirebaseAuth.instance.currentUser();
-    CatchMeApp.userUid = user.uid;
-    return user;
-  }
+    Future<AuthCredential> _getGoogleCredential() async {
+        GoogleSignInAccount googleSignInAccount = await _gSignIn.signIn();
+        GoogleSignInAuthentication authentication =
+            await googleSignInAccount.authentication;
 
-  void signOut() {
-    _gSignIn.signOut();
-  }
+        return GoogleAuthProvider.getCredential(
+            idToken: authentication.idToken,
+            accessToken: authentication.accessToken
+        );
+    }
+
+    Future<FirebaseUser> initUserId() async {
+        var user = await FirebaseAuth.instance.currentUser();
+        CatchMeApp.userUid = user.uid;
+        return user;
+    }
+
+    void signOut() {
+        _gSignIn.signOut();
+    }
 }
