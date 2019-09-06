26ae0680b4090eb9331098ff4ae3a81e5ce6359e
Kolya Grebnev <mumiyasss@gmail.com>
2019-09-06 23:32:15 +0300

    Profile image placeholder with random color.

--- lib/ui/Widgets.dart
+++ lib/ui/Widgets.dart
@@ -1,27 +1,44 @@
+import 'dart:math';
+
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class Widgets {
-  static Widget profilePicture(
-      BuildContext context, String photoUrl, double sizeProportion) {
-    double size = MediaQuery.of(context).size.width * sizeProportion;
-    return photoUrl == null
-        ? SvgPicture.asset(
-            'assets/profile.svg',
+    static Widget profilePicture(BuildContext context, String photoUrl,
+        double sizeProportion) {
+        double size = MediaQuery
+            .of(context)
+            .size
+            .width * sizeProportion;
+        return Container(
             width: size,
             height: size,
-          )
-        : Container(
-            width: size,
-            height: size,
-            decoration: BoxDecoration(
-                
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: CachedNetworkImageProvider(photoUrl))),
-            //child: profilePhoto
-          );
-  }
+            child: (photoUrl == null)
+                ? SvgPicture.asset('assets/profile.svg')
+                : CachedNetworkImage(
+                imageUrl: photoUrl,
+                width: size,
+                height: size,
+                imageBuilder: (context, imageProvider) =>
+                    Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: imageProvider
+                            ),
+                        ),
+                    ),
+                placeholder: (context, url) =>
+                    Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(Random().nextInt(0xFFFFFFFF)),
+                        ),
+                    ),
+            ),
+        );
+    }
 }
