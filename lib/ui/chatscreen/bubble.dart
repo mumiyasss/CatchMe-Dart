f0cb3b3f8290e8c1d5189315c3a8c7197acdf4cd
Kolya Grebnev <mumiyasss@gmail.com>
2019-09-07 00:56:35 +0300

    Image decoration and placeholder in messages panel

--- lib/ui/chatscreen/bubble.dart
+++ lib/ui/chatscreen/bubble.dart
@@ -19,26 +19,50 @@ class MessageBubble extends StatelessWidget {
             .width * 0.3;
         var bottomMargin = 9.0;
 
-        var messageBubble = Container(
+        var messageBubble = (_message.imageUrl == null) ?
+        Container(
             padding: EdgeInsets.all(10),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.all(Radius.circular(20)),
                 border: Border.all(color: Color(self ? 0xFFCECECE : 0)),
                 color: Color(self ? 0xFFFFFFFF : 0xFF2196F3),
             ),
-            child: Column(
-                children: <Widget>[
-                    _message.imageUrl != null ? CachedNetworkImage(
-                        imageUrl: _message.imageUrl) :
-                    Text(
-                        _message.text,
-                        textAlign: TextAlign.justify,
-                        style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 16,
-                            color: self ? Colors.black : Colors.white),
+            child: Text(
+                _message.text,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    color: self ? Colors.black : Colors.white),
+            ),
+
+        ) : Container(
+            height: 200,
+            child: CachedNetworkImage(
+                imageUrl: _message.imageUrl,
+                imageBuilder: (context, imageProvider) =>
+                    Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            //border: Border.all(color: Color(self ? 0xFFCECECE : 0)),
+                            color: Color(self ? 0xFFFFFFFF : 0xFF2196F3),
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: imageProvider
+                            ),
+                        ),
                     ),
-                ],
+                placeholder: (context, url) =>
+                    Container(
+                        width: 266,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            border: Border.all(color: Color(self ? 0xFFCECECE : 0)),
+                            color: Color(self ? 0xFFFFFFFF : 0xFF2196F3),
+                        ),
+                        child: Center(child: CircularProgressIndicator()),
+                    ),
+
             ),
         );
 
